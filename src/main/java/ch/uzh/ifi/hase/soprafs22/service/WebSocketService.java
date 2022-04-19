63452d87f0cadb715610c75e6a291fa646e26580
Dennys <dennys.huber@pm.me>
2022-04-19 08:04:18 +0200

    added backbone for sending messages via websocktes

--- src/main/java/ch/uzh/ifi/hase/soprafs22/service/WebSocketService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/service/WebSocketService.java
@@ -1,4 +1,47 @@
 package ch.uzh.ifi.hase.soprafs22.service;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ch.uzh.ifi.hase.soprafs22.entity.Player;
+import ch.uzh.ifi.hase.soprafs22.repository.PlayerRepository;
+
+@Service
+@Transactional
 public class WebSocketService {
+
+	Logger log = LoggerFactory.getLogger(WebSocketService.class);
+
+	protected final PlayerRepository playerRepo;
+	private final PlayerService playerService;
+
+	public WebSocketService(@Qualifier("playerRepository") PlayerRepository playerRepository,
+			@Lazy PlayerService playerService) {
+		this.playerRepo = playerRepository;
+		this.playerService = playerService;
+	}
+
+	@Autowired // Automatic injection of beans
+	protected SimpMessagingTemplate smesg;
+
+	protected void convertAndSendToPlayer(Long id, String path, Object dto) {
+		String stringId = Long.toString(id);
+		this.smesg.convertAndSendToUser(stringId, path, dto);
+	}
+
+	protected void convertAndSendToAllInLobby(String path, String dest, Object dto, long lobbyId) {
+		List<Player> lobby = this.playerRepo.findByLobbyId(lobbyId);
+		for (Player player : lobby) {
+			convertAndSendToPlayer(player.getId(), path + lobbyId + dest, dto);
+		}
+	}
+
 }
