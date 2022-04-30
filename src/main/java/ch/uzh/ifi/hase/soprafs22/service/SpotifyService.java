79eb3ce14840c9a9c068141d43eda7a9932d0008
v4lentin1879 <valentin@berninabahn.com>
2022-04-30 13:52:26 +0200

    test deployment

--- src/main/java/ch/uzh/ifi/hase/soprafs22/service/SpotifyService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/service/SpotifyService.java
@@ -1,7 +1,9 @@
 package ch.uzh.ifi.hase.soprafs22.service;
 
+
 import ch.uzh.ifi.hase.soprafs22.rest.dto.SpotifyPostDTO;
 import ch.uzh.ifi.hase.soprafs22.spotify.GetUsersTopArtistsAndTracksExample;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import se.michaelthelin.spotify.SpotifyApi;
@@ -20,7 +22,9 @@ import static ch.uzh.ifi.hase.soprafs22.spotify.authorization.AuthorizationCode.
 public class SpotifyService {
     //private static final String accessToken = "BQAaj-lzAbhmapRS58RRVg0BXxj_HyCIAZdYYS_u6avym2zx   bCNPTWDb4P8QI-c7d_qF1T0m7gZzCbjohjjO9cAAhjSg9v6mhgphr9ankW6Dk8BRh_ns_SD4vLnin5DwxFbqVeiZoeVi_osHgjq4NliiVZXq";
     private static final String clientId = "d7d44473ad6a47cd86c580fcee015449";
-    private static final String clientSecret = "";
+
+    @Value("${clientSecret}")
+    private static final String clientSecret;
     private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:3000/connectspotify");
 
 
