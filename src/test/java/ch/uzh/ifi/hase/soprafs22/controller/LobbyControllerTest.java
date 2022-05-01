0d2003848f09e12448597382a26c82120f3a80da
v4lentin1879 <valentin@berninabahn.com>
2022-05-01 13:11:48 +0200

    some more tests

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/controller/LobbyControllerTest.java
@@ -0,0 +1,116 @@
+package ch.uzh.ifi.hase.soprafs22.controller;
+
+import ch.uzh.ifi.hase.soprafs22.repository.GameRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.BeforeMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.web.server.ResponseStatusException;
+
+import ch.uzh.ifi.hase.soprafs22.constant.SongPool;
+import ch.uzh.ifi.hase.soprafs22.entity.Game;
+import ch.uzh.ifi.hase.soprafs22.entity.Player;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.LobbyIdDTO;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.PlayerPostDTO;
+import ch.uzh.ifi.hase.soprafs22.service.GameService;
+import ch.uzh.ifi.hase.soprafs22.service.PlayerService;
+import ch.uzh.ifi.hase.soprafs22.service.SpotifyService;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+@WebMvcTest(LobbyController.class)
+public class LobbyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Player player;
+    private Game game;
+
+    @MockBean
+    private PlayerService playerService;
+
+    @MockBean
+    private GameService gameService;
+
+    @MockBean
+    private SpotifyService spotifyService;
+
+    /**
+     * Helper Method to convert userPostDTO into a JSON string such that the input
+     * can be processed
+     * Input will look like this: {"name": "Test User", "username": "testUsername"}
+     *
+     * @param object
+     * @return string
+     */
+    private String asJsonString(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    String.format("The request body could not be created.%s", e.toString()));
+        }
+    }
+
+    @BeforeEach
+    void setup() {
+        player = new Player();
+        player.setPlayerName("playerName");
+        player.setLobbyId(1L);
+
+        game = new Game(spotifyService, SongPool.ROCK);
+    }
+
+    @Test
+    public void createLobbyPOSTTest() throws Exception {
+        LobbyIdDTO lobbyIdDTO = new LobbyIdDTO();
+        lobbyIdDTO.setLobbyId(1);
+
+        given(gameService.createNewLobby(spotifyService)).willReturn(1);
+
+        MockHttpServletRequestBuilder postRequest = post("/lobbies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(lobbyIdDTO));
+
+        mockMvc.perform(postRequest).andExpect(status().isCreated()).andExpect(jsonPath("$.lobbyId", is(1)));
+    }
+/*
+    @Test
+    public void createPlayerPOSTTest() throws Exception {
+    PlayerPostDTO playerPostDTO = new PlayerPostDTO();
+    playerPostDTO.setPlayerName("playerName");
+
+    GameRepository.addGame(1, game);
+    given(playerService.addPlayer(player)).willReturn(player);
+    long id = player.getlobbyId();
+
+
+    MockHttpServletRequestBuilder postRequest = post("/lobbies/1")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(asJsonString(playerPostDTO));
+
+    mockMvc.perform(postRequest).andExpect(status().isCreated())
+            .andExpect(jsonPath("$.playerName", is(player.getPlayerName())));
+
+     }*/
+
+}
---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/controller/SpotifyAuthControllerTest.java
@@ -0,0 +1,87 @@
+package ch.uzh.ifi.hase.soprafs22.controller;
+
+
+import ch.uzh.ifi.hase.soprafs22.rest.dto.SpotifyAuthCodeGetDTO;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.SpotifyPostDTO;
+import ch.uzh.ifi.hase.soprafs22.service.SpotifyService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * RestController
+ * This is a WebMvcTest which allows to test the RESTController i.e. GET/POST/PUT request without actually sending them over the network.
+ * This tests if the RESTController works.
+ */
+@WebMvcTest(SpotifyAuthController.class)
+public class SpotifyAuthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SpotifyService spotifyService;
+
+    @Test
+    void generateAuthorizationCodeUriTest() throws Exception{
+        SpotifyAuthCodeGetDTO spotifyAuthCodeGetDTO = new SpotifyAuthCodeGetDTO();
+        spotifyAuthCodeGetDTO.setRedirectionURL("https://accounts.spotify.com:443/authorize?client_id=d7d44473ad6a47cd86c580fcee015449&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fconnectspotify&scope=user-read-private%2Cplaylist-read-private%2Cuser-library-read%2Cuser-top-read%2Cuser-read-recently-played");
+
+        given(spotifyService.authorizationCodeUri()).willReturn("https://accounts.spotify.com:443/authorize?client_id=d7d44473ad6a47cd86c580fcee015449&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fconnectspotify&scope=user-read-private%2Cplaylist-read-private%2Cuser-library-read%2Cuser-top-read%2Cuser-read-recently-played");
+
+        // when
+        MockHttpServletRequestBuilder getRequest = get("/Spotify/authorizationCodeUri").contentType(MediaType.APPLICATION_JSON);
+        // then
+        mockMvc.perform(getRequest).andExpect(status().isOk())
+                .andExpect(jsonPath("$.redirectionURL", is(spotifyAuthCodeGetDTO.getRedirectionURL())));
+    }
+
+    @Test
+    void getAuthorizationCodeTest() throws Exception {
+        SpotifyPostDTO spotifyPostDTO = new SpotifyPostDTO();
+        spotifyPostDTO.setCode("ThisIsAnAuthorizationCode");
+
+        String accessToken = "ThisIsAnAccessToken";
+
+        //spotifyService.authorizationCode(spotifyPostDTO);
+        given(spotifyService.getAccessToken()).willReturn(accessToken);
+
+        MockHttpServletRequestBuilder postRequest = post("/Spotify/authorizationCode").contentType(MediaType.APPLICATION_JSON)
+                                                        .content(asJsonString(spotifyPostDTO));;
+
+        mockMvc.perform(postRequest).andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken", is(accessToken)));
+    }
+
+    /**
+     * Helper Method to convert Object into a JSON string such that the input can be processed
+     * Input will look like this: {"name": "Test User", "username": "testUsername"}
+     *
+     * @param object
+     * @return string
+     */
+    private String asJsonString(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        }
+        catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("The request body could not be created.%s", e.toString()));
+        }
+    }
+}
