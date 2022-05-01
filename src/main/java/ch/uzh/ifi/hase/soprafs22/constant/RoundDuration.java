550921cc8682e07a5affce9a06e308536560eeb1
bellachesney <76061035+bellachesney@users.noreply.github.com>
2022-05-01 11:38:19 +0200

    update

--- src/main/java/ch/uzh/ifi/hase/soprafs22/constant/RoundDuration.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/constant/RoundDuration.java
@@ -1,7 +1,7 @@
 package ch.uzh.ifi.hase.soprafs22.constant;
 
 public enum RoundDuration {
-    THREE(3), TEN(10), TWELVE(12), FOURTEEN(14), SIXTEEN(16), EIGHTEEN(18), TWENTY(20);
+    TEN(10), TWELVE(12), FOURTEEN(14), SIXTEEN(16), EIGHTEEN(18), TWENTY(20);
 
     private final int value;
 
---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/SpotifyServiceTest.java
@@ -0,0 +1,5 @@
+package ch.uzh.ifi.hase.soprafs22.service;
+
+public class SpotifyServiceTest {
+
+}
