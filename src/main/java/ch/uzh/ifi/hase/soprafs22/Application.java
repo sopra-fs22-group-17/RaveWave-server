04c62f59c0c5ff8adbb1526f4d49606b417bc963
v4lentin1879 <valentin@berninabahn.com>
2022-05-11 18:46:50 +0200

    heroku crash debugging

--- src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
@@ -1,7 +1,9 @@
 package ch.uzh.ifi.hase.soprafs22;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RestController;
