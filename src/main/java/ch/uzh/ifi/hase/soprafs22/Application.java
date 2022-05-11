80672ac815fced306e92e4b3015049a84fa818c7
v4lentin1879 <valentin@berninabahn.com>
2022-05-11 11:26:10 +0200

    persistent database?

--- src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.hase.soprafs22;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
