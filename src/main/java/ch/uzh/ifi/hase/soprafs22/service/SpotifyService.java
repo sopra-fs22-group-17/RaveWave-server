de13ce31ff7ce9d1004fe03763bd970e45dd418f
Valentin <75266614+v4lentin1879@users.noreply.github.com>
2022-05-20 11:36:20 +0200

    Update SpotifyService.java
    changed redirectURL

--- src/main/java/ch/uzh/ifi/hase/soprafs22/service/SpotifyService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/service/SpotifyService.java
@@ -34,7 +34,7 @@ public class SpotifyService {
 
     private static final String clientSecret = System.getenv("clientSecret");
     private static final URI redirectUri = SpotifyHttpManager
-            .makeUri("https://sopra-fs22-group17-clientv3.herokuapp.com/selectgamemode");
+            .makeUri(System.getenv(“redirectURL”));
     private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
             .setClientId(clientId)
             .setClientSecret(clientSecret)
